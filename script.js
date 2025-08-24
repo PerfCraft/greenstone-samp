26135493fabc211a0ba7d80aaefb82103f047a37
PerfCraft <97217802+PerfCraft@users.noreply.github.com>
2025-08-25 01:42:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = "https://api.g-stone.ro/samp/";
+const API_URL = "https://api.g-stone.r/samp/";
 
 function updateDashboard() {
 
@@ -36,5 +36,6 @@ function updateDashboard() {
 
 updateDashboard();
 setInterval(updateDashboard, 1000);
+
 
 
